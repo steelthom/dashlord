c4aa092be33229d5c8f424c6454dc9072bc72636
Quentin Lienhardt <quentin.lienhardt@actimage.com>
2025-08-14 14:55:04 +0200

    Remove again some urls & www if not exists

--- check_urls.py
+++ check_urls.py
@@ -6,15 +6,16 @@ with open("urls.txt", "r", encoding="utf-8") as file:
 for url in urls:
     url = url.strip()
     try:
-        response = requests.get(url, timeout=20)
+        response = requests.get(url, timeout=15)
     except requests.RequestException as e:
         if "NameResolutionError" in str(e):
+            print(f"Delete URL: {url}")
             with open("urls.txt", "r", encoding="utf-8") as file:
                 lines = file.readlines()
             with open("urls.txt", "w", encoding="utf-8") as file:
                 for line in lines:
                     if line.strip() != url:
                         file.write(line)
-            print(f"Deleted URL: {url}")
         else:
             print(f"Error accessing {url}")
+            print(e)
