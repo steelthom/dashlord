fb9d43747938abfc05afea7c5e377a59eabac969
Quentin Lienhardt <quentin.lienhardt@actimage.com>
2025-08-28 11:07:12 +0200

    Clean or prefer www

--- check_urls.py
+++ check_urls.py
@@ -1,6 +1,6 @@
 import requests
 
-with open("urls.txt", "r", encoding="utf-8") as file:
+with open("urls.txt", encoding="utf-8") as file:
     urls = file.readlines()
 
 for url in urls:
@@ -10,12 +10,23 @@ for url in urls:
     except requests.RequestException as e:
         if "NameResolutionError" in str(e):
             print(f"Delete URL: {url}")
-            with open("urls.txt", "r", encoding="utf-8") as file:
-                lines = file.readlines()
-            with open("urls.txt", "w", encoding="utf-8") as file:
-                for line in lines:
-                    if line.strip() != url:
+            try:
+                response = requests.get(url.replace("://", "://www."), timeout=15)
+                with open("urls.txt", encoding="utf-8") as file:
+                    lines = file.readlines()
+                with open("urls.txt", "w", encoding="utf-8") as file:
+                    for line in lines:
+                        if line.strip() == url:
+                            line = line.replace(url, url.replace("://", "://www."))
                         file.write(line)
+            except requests.RequestException:
+                print(f"Also failed with www: {url}")
+                with open("urls.txt", encoding="utf-8") as file:
+                    lines = file.readlines()
+                with open("urls.txt", "w", encoding="utf-8") as file:
+                    for line in lines:
+                        if line.strip() != url:
+                            file.write(line)
         else:
             print(f"Error accessing {url}")
             print(e)
